32d3b160d9b19f9e285ce7572024544212a66a4e
John Werner <jwerner547@gmail.com>
2021-03-02 09:07:53 -0700

    refactor repositories and their interfaces

---
+++ BookStore/BookStore.DataAccess/ProductRepository.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using BookStore.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BookStore.DataAccess
+{
+    class ProductRepository : IProductRepository
+    {
+        /// <summary>
+        /// Generates the context for accessing the database based on options that are given.
+        /// </summary>
+        /// <param name="logStream"></param>
+        /// <returns> bookstoredbContext _context </returns>
+        private bookstoredbContext GenerateDBContext(StreamWriter logStream)
+        {
+            string connString = File.ReadAllText("C:/revature/bkdb.txt");
+            DbContextOptions<bookstoredbContext> options = new DbContextOptionsBuilder<bookstoredbContext>()
+                .UseSqlServer(connString)
+                .LogTo(logStream.WriteLine, minimumLevel: LogLevel.Information)
+                .Options;
+
+            return new bookstoredbContext(options);
+        }
+
+        // CRUD Product
+
+        /// <summary>
+        /// Returns the list of all Products in the database.
+        /// </summary>
+        /// <returns> List<Product> list </returns>
+        public List<Domain.Product> GetAllProducts()
+        {
+            using var logStream = new StreamWriter("bkdb-logs.txt", append: false) { AutoFlush = true };
+            using var _context = GenerateDBContext(logStream);
+
+            var x = _context.Set<Product>().AsEnumerable();
+            List<Domain.Product> list = new List<Domain.Product>();
+
+            foreach (var i in x)
+            {
+                var p = new Domain.Product(i.Name, (decimal)i.Price) { ID = i.Id };
+                list.Add(p);
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Returns the specific Product that has the given ID.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> Product x </returns>
+        public Domain.Product GetProductByID(int id)
+        {
+            using var logStream = new StreamWriter("bkdb-logs.txt", append: false) { AutoFlush = true };
+            using var _context = GenerateDBContext(logStream);
+
+            var c = _context.Set<Product>().Find(id);
+            var x = new Domain.Product(c.Name, (decimal)c.Price);
+
+            return x;
+        }
+
+        /// <summary>
+        /// Returns the list of products that match the given name.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns> List<Product> toReturn </returns>
+        public List<Domain.Product> GetProductByName(string name)
+        {
+            using var logStream = new StreamWriter("bkdb-logs.txt", append: false) { AutoFlush = true };
+            using var _context = GenerateDBContext(logStream);
+
+            var products = _context.Set<Product>().Where(x => name == x.Name).ToList();
+            List<Domain.Product> toReturn = new List<Domain.Product>();
+
+            foreach (var p in products)
+            {
+                var x = new Domain.Product(p.Name, (decimal)p.Price) { ID = p.Id };
+                toReturn.Add(x);
+            }
+
+            return toReturn;
+        }
+
+        /// <summary>
+        /// Adds the given Product to the database.
+        /// </summary>
+        /// <param name="p"></param>
+        public void AddProduct(Domain.Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Updates the given Product in the database.
+        /// </summary>
+        /// <param name="p"></param>
+        public void UpdateProduct(Domain.Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deletes the given Product from the database.
+        /// </summary>
+        /// <param name="p"></param>
+        public void DeleteProduct(Domain.Product p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BookStore/BookStore.Web/Startup.cs
+++ BookStore/BookStore.Web/Startup.cs
@@ -1,21 +1,29 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace BookStore.Web
 {
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStore.WebUI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +32,19 @@ namespace BookStore.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore.WebUI v1"));
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
