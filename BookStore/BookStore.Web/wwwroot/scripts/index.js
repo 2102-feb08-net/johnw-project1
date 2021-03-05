1a0e24bc524a8702cfa64bc56d299ee9e23bd572
John Werner <jwerner547@gmail.com>
2021-03-05 09:52:21 -0700

    fixing some bugs according to sonarcloud, and adding some functionality towards adding a customer

--- BookStore/BookStore.Web/wwwroot/scripts/index.js
+++ BookStore/BookStore.Web/wwwroot/scripts/index.js
@@ -8,7 +8,6 @@ function loadCustomerData() {
             if (!resp.ok) {
                 throw new Error(`failed to get customers (${resp.status})`);
             }
-            console.log(resp);
             return resp.json();
         });
 }
@@ -29,3 +28,41 @@ loadCustomerData()
         // handle the error
     });
 
+var locations;
+function loadLocationData() {
+    return fetch('/api/locations')
+        .then(resp => {
+            if (!resp.ok) {
+                throw new Error(`failed to get locations (${resp.status})`);
+            }
+            return resp.json();
+        });
+}
+
+loadLocationData()
+    .then(locs => {
+        console.log(locs);
+        locations = locs;
+        console.log(locations);
+        let locIDSelect = document.getElementById("defaultLocId");
+        for (const l of locations) {
+            console.log(l);
+            let o = document.createElement("option");
+            o.text = l.name;
+            o.value = `${l.id}`;
+            locIDSelect.appendChild(o);
+        }
+    });
+
+const submitBtn = document.getElementById("addCustomer");
+addCustomer.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    let c = {
+        firstName: document.getElementById("customer-firstName").textContent,
+        lastName: document.getElementById("customer-lastName").textContent,
+        defaultLocationId: document.getElementById("defaultLocId").value
+    }
+
+    console.log(c);
+});
