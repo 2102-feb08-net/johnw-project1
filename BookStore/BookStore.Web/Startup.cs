1b178e4695a28f2edbb539d07d37b962bdc0a2ac
John Werner <jwerner547@gmail.com>
2021-03-05 12:56:32 -0700

    fixing the configuration so the deployment works...

--- BookStore/BookStore.Web/Startup.cs
+++ BookStore/BookStore.Web/Startup.cs
@@ -20,10 +20,17 @@ namespace BookStore.Web
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connectionString = File.ReadAllText("C:/revature/bkdb.txt");
+            string connectionString = Configuration["ConnectionStrings:BookStoreDB"];
 
             services.AddDbContext<bookstoredbContext>(options =>
             {
